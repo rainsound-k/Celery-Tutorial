5adc7a89488f46ea081a8dc25fe591fc3ed9cdbf
rainsound-k <rainsound128@gmail.com>
2018-09-28 10:13:01 +0900

    Post 모델 생성 및 makemigrations, blog_post_tast 태스크 생성

--- app/blog/tasks.py
+++ app/blog/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 
+from .models import Post
 from config.celery import app
 
 
@@ -11,3 +12,13 @@ def blog_debug_task(self):
 @shared_task
 def blog_shared_task():
     return 'shared'
+
+
+@shared_task
+def blog_post_task():
+    for i in range(100000):
+        Post.objects.create(
+            title=f'{i} 번째 글입니다.',
+            content=f'{i} 글의 내용입니다!!!!!!!!!!'
+        )
+    return
